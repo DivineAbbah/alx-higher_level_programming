281ec9585a6d90ccfe01d598e40c1bd402223b11
Abbah Divine <abbahattamah@gmail.com>
2023-05-18 05:25:32 -0700

    deleting of databases

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- A script that deletes the DB hbtn_0c_0
+-- If the DB hbtn_0c_0 doesn’t exist, the script should not fail
+-- not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
