04ad397b191aec979e43c3a4ed29a3d332f8f0db
Abbah Divine <abbahattamah@gmail.com>
2023-05-18 13:00:16 -0700

    10-top_score.sql update;

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
---script to list the score,name from the second_table in order of descinding
---results should display bothe the score and the name in desc order
+-- script to list the score,name from the second_table in order of descinding
+-- results should display bothe the score and the name in desc order
 
 SELECT score, name FROM second_table ORDER BY score DESC;
