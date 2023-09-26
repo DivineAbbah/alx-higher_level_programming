f5524712edaada12afe065992457fd3430ea42f3
Divine Abbah <abbahattamah@gmail.com>
2023-09-26 01:46:07 -0700

    code style check

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* print status code of a request */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response) => {
+  if (err) console.log(err);
+  else console.log('code:', response.statusCode);
+});
